74907f3a52f78772e25e09bf28cc22e2f7a2ee27
GabiRezende <gr.gabrielarezende@gmail.com>
2022-06-16 20:31:37 -0300

    resolução do exercício 1

---
+++ estruturasdedados.js
@@ -0,0 +1,10 @@
+let nome = "Gabriela Rezende";
+
+let num = 13;
+conta = 2022 - num;
+console.log(conta+num);
+
+console.log("Estou mexendo no console.log");
+
+let quartaFeira = true;
+console.log(quartaFeira);
