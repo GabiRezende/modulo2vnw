a18c645fcdab12bb22f78a79e4cadbb1224a48a4
GabiRezende <gr.gabrielarezende@gmail.com>
2022-06-18 01:10:21 -0300

    acrescentei a resolução da parte bônus do exercício 2

--- ifelse.js
+++ ifelse.js
@@ -1,6 +1,8 @@
 let idade = 19;
 let humano = true;
 let mesAniversario = "Dezembro";
+let nome = "Gabi";
+let sobrenome = "Rezende";
 
 if (idade > 18) {
     console.log('Maior de 18 anos')
@@ -18,5 +20,17 @@ if (mesAniversario === "Janeiro" || "Dezembro") {
     console.log('Seu aniversário é em Janeiro ou Dezembro')
 } else {
     console.log('Seu aniversário não é em Dezembro ou Janeiro')
+}
+
+if (nome.substring(0,1) === "R") {
+    console.log('Seu nome começa com a letra R')
+} else {
+    console.log('Seu nome não começa com a letra R')
+}
+
+if (sobrenome.length > 6 || nome.substring(0,1) === "E") {
+    console.log('Seu sobrenome tem mais de 6 letras ou seu nome começa com a letra E')
+} else {
+    console.log('Seu sobrenome não tem mais de 6 letras e seu nome não começa com a letra E')
 }
 
